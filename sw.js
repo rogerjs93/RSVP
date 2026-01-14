771a5bd49a8ccd3415a97fc54fd2cd54c22b2fe7
Roger Salas <rogerjs93@gmail.com>
2026-01-14 17:49:04 +0200

    Hide scrollbar in research modal UI

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
  * 100% Open Source - MIT License
  */
 
-const CACHE_NAME = 'fastreader-v3';
-const CACHE_VERSION = 3;
+const CACHE_NAME = 'fastreader-v4';
+const CACHE_VERSION = 4;
 
 // Assets to cache on install
 const PRECACHE_ASSETS = [
